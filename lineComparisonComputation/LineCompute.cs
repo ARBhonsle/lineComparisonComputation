c16dd9536ec18e216a5bbc2b6ce56ffa2e57e0bb
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-07-22 15:52:17 +0530

    [aditi] Refactor: Line lengths Checked for equality

--- lineComparisonComputation/LineCompute.cs
+++ lineComparisonComputation/LineCompute.cs
@@ -6,63 +6,58 @@ namespace lineComparisonComputation
 {
     class LineCompute: ILineCompute
     {
+        readonly int[] point = new int[4];
+        readonly int[] point1 = new int[4];
+        public LineCompute(int[] point)
+        {
+            for (int i = 0; i < point.Length; i++)
+            {
+                this.point[i] = point[i];
+            }
+        }
+        public LineCompute(int[] point,int[] point1)
+        {
+            for (int i = 0; i < point.Length; i++)
+            {
+                this.point[i] = point[i];
+                this.point1[i] = point1[i];
+            }
+        }
         public void LengthCalc()
         {
-            int[] x = new int[2];
-            int[] y= new int[2];
-            Console.WriteLine("Length calculation between two points in a line");
-            for(int i = 0; i < 2; i++)
-            {
-                Console.WriteLine("Give "+(i+1)+" point coordinate:");
-                Console.WriteLine("X: ");
-                x[i] = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Y: ");
-                y[i] = Convert.ToInt32(Console.ReadLine());
-            }
-            Double lineLength = Math.Sqrt(Math.Pow(x[0] - x[1], 2) + Math.Pow(y[0] - y[1], 2));
+            Double lineLength = Math.Sqrt(Math.Pow(point[0] - point[2], 2) + Math.Pow(point[1] - point[3], 2));
             Console.WriteLine("Length of line (within given points) :"+ lineLength);
         }
         public void CompareLength()
         {
-            int[] x=new int[4];
-            int[] y = new int[4];
-            int j = 0;
-            for (int i = 0; i < 4; i++)
+            if(this.point[0].Equals(this.point[2]) && this.point[1].Equals(this.point[3]))
             {
-                Console.WriteLine("Give Coordinate for Line "+(j + 1));
-                Console.WriteLine("X:");
-                x[i] = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Y: ");
-                y[i]= Convert.ToInt32(Console.ReadLine());
-            }
-            if(x[0].Equals(x[2]) && x[1].Equals(x[3]))
-            {
-                if (y[0].Equals(y[2]) && y[1].Equals(y[3]))
+                if (this.point1[0].Equals(this.point1[2]) && this.point1[1].Equals(this.point1[3]))
                 {
-                    Console.WriteLine("Line Equal");
+                    Console.WriteLine("Line lengths Equal");
                 }
                 else 
                 {
-                    Console.WriteLine("Line Length Not Equal");
+                    Console.WriteLine("Line Lengths Not Equal");
                 }
-            }else if (x[0].Equals(x[3]) && x[1].Equals(x[2]))
+            }else if (this.point[0].Equals(this.point[3]) && this.point[1].Equals(this.point[2]))
             {
-                if(y[0].Equals(y[3]) && y[1].Equals(y[2]))
+                if(this.point1[0].Equals(this.point1[3]) && this.point1[1].Equals(this.point1[2]))
                 {
-                    Console.WriteLine("Line Equal");
+                    Console.WriteLine("Line lengths Equal");
                 }
                 else
                 {
-                    Console.WriteLine("Line Length Not Equal");
+                    Console.WriteLine("Line Lengths Not Equal");
                 }
             }
             else
             {
-                Double lengthLine1 = Math.Sqrt(Math.Pow(x[0] - x[1], 2) + Math.Pow(y[0] - y[1], 2));
-                Double lengthLine2 = Math.Sqrt(Math.Pow(x[2] - x[3], 2) + Math.Pow(y[2] - y[3], 2));
+                Double lengthLine1 = Math.Sqrt(Math.Pow(this.point[0] - this.point[1], 2) + Math.Pow(this.point1[0] - this.point1[1], 2));
+                Double lengthLine2 = Math.Sqrt(Math.Pow(this.point[2] - this.point[3], 2) + Math.Pow(this.point1[2] - this.point1[3], 2));
                 if (lengthLine1 != lengthLine2)
                 {
-                    Console.WriteLine("Line Length Not Equal");
+                    Console.WriteLine("Line Lengths Not Equal");
                 }
                 else
                 {
--- lineComparisonComputation/Program.cs
+++ lineComparisonComputation/Program.cs
@@ -7,8 +7,20 @@ namespace lineComparisonComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program!");
-            ILineCompute line = new LineCompute();
-            //line.LengthCalc();
+            Console.WriteLine("Length calculation between two points in a line");
+            int[] point = new int[4];
+            int[] point1 = new int[4];
+            int j;
+            for (int i=0;i<4;i++) 
+            {
+                j = (i < 2) ? 1 : 2;
+                Console.WriteLine("Give "+j+" point coordinate: "+(i%2+1));
+                Console.WriteLine("X: ");
+                point[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Y: ");
+                point1[i] = Convert.ToInt32(Console.ReadLine());                
+            }
+            ILineCompute line = new LineCompute(point,point1);
             line.CompareLength();
         }
     }
